167f95a14fd3526012358a2981da05e27eb65e66
Kirill Pastukhov <kirush-ka@yandex.ru>
2022-11-23 19:57:42 +0200

    Split main into two scripts

---
+++ read_chat.py
@@ -0,0 +1,22 @@
+import argparse
+import asyncio
+
+from src.chat_connector import chat_connector
+from src.listen import listen
+
+
+async def main():
+    parser = argparse.ArgumentParser(description="Client settings")
+    parser.add_argument("--host", dest="host", type=str, default="minechat.dvmn.org", help="Host to listen")
+    parser.add_argument("--port", dest="port", type=str, default="5000", help="Port")
+    parser.add_argument("--history", dest="history_path", type=str, default="minechat.history", help="File for message history.")
+    args = parser.parse_args()
+
+    async with chat_connector(args.host, args.port) as conn:
+        reader, _ = conn
+        await listen(reader, args.history_path)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- write_chat.py
+++ write_chat.py
@@ -4,7 +4,7 @@ import asyncio
 from loguru import logger
 
 from src.authorise import authorise
-from src.listen import listen
+from src.chat_connector import chat_connector
 from src.register import register
 from src.write import submit_message
 
@@ -13,24 +13,23 @@ async def main():
     parser = argparse.ArgumentParser(description="Client settings")
     parser.add_argument("--message", dest="message", type=str, required=True, help="Message to write")
     parser.add_argument("--host", dest="host", type=str, default="minechat.dvmn.org", help="Host to listen")
-    parser.add_argument("--port", dest="port", type=str, default="5000", help="Port")
+    parser.add_argument("--port", dest="port", type=str, default="5050", help="Port")
     parser.add_argument("--token", dest="token", type=str, help="Chat access token")
     parser.add_argument("--username", dest="username", type=str, help="Chat username")
     parser.add_argument("--history", dest="history_path", type=str, default="minechat.history", help="File for message history.")
     args = parser.parse_args()
 
     if not args.token:
-        response = await register(args.host, args.username)
+        response = await register(args.host, args.port, args.username)
         args.token = response["account_hash"]
 
-    msg_writer, response = await authorise(args.token)
-    if response is None:
-        logger.info("Incorrect token.")
-        quit()
+    async with chat_connector(args.host, args.port) as conn:
+        reader, writer = conn
+        await authorise(reader, writer, args.token)
+        logger.info("Authorization was successful")
 
-    await submit_message(args.message, msg_writer)
-
-    await listen(args.host, args.port, args.history_path)
+        await submit_message(args.message, writer)
+        logger.info("Message was sent")
 
 
 if __name__ == "__main__":
